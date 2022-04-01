0578117477df9937bc61cffc5718de1adf2fa88e
Rémy Pottier <Remypottierpro@gmail.com>
2022-04-01 13:18:52 +0200

    :rocket: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="z-[999] fixed bottom-20 right-8">
+    <div>
+      hello world
+    </div>
+  </div>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import './index.css'
+
+const app = createApp(App);
+
+app.use(createPinia());
+const relazonContainer = window.document.createElement('div')
+relazonContainer.id="relazonContainer"
+document.body.appendChild(relazonContainer)
+app.mount("#relazonContainer");
